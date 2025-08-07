8932daa4fdff83e85ae22890328de3b3aa3d9690
Katarina Uhrinova <uhrinova.kata@gmail.com>
2025-08-07 08:00:40 +0200

    feat: Add Page Object Model for login and dashboard functionality
    - Created LoginPage and DashboardPage classes for better test organization.
    - Implemented methods for user login and logout actions.
    - Added tests for login/logout functionality using Page Objects.
    - Introduced random data generation test using Faker.js for improved test data handling.
    - Updated package.json with new dependencies for ESLint and TypeScript.
    - Configured TypeScript with tsconfig.json for better type checking and module resolution.

--- tests/learning/first-scripts/first_tests.spec.ts
+++ tests/learning/first-scripts/first_tests.spec.ts
@@ -1,12 +1,12 @@
-import { test } from "@playwright/test";
+/* import { test } from "@playwright/test";
 test("First Test", async ({ page }) => {
   await page.goto("https://tredgate.com/pmtool");
   await page.locator("#username").fill("pw_academy");
   await page.locator("#password").fill("Playwright321!");
   await page.locator(".btn").click();
 });
-
-/*  import { test } from "@playwright/test"; - toto tam musí byt vždy !!!! 
+---------------------------------------------------------------------------------------------
+ import { test } from "@playwright/test"; - toto tam musí byt vždy !!!! 
 test("First Test", async ({ page }) => { - test je funkcia, ktorá spúšťa testy - kroky sú v {} 
     await = čaká na načítanie stránky
     page = vraví že má komunikovať so stránkou / prehliadačom - dáva sa vzdy ked pracujem s prehliadačom !!! 
@@ -20,4 +20,33 @@ test("First Test", async ({ page }) => { - test je funkcia, ktorá spúšťa tes
 Spúštanie testov:
     1. Terminál: npx playwright test
    
+*/
+// tests/learning/page-objects
+// page_objects_tests.spec.ts
+import { test } from "@playwright/test";
+import { LoginPage } from "../../../src/pages/pmtool/login_page.ts";
+
+test("Page Objects Tests", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.openPmtool();
+  await loginPage.fillUsername("pw_academy");
+  await loginPage.fillPassword("Playwright321!");
+  await loginPage.clickLogin();
+});
+
+test("Page Objects - general method", async ({ page }) => {
+  const loginPage = new LoginPage(page);
+  await loginPage.openPmtool();
+  await loginPage.login("pw_academy", "Playwright321!");
+});
+
+/*
+Cvičení - Vytvořte nový test a PageObject výkazy ⌛15:00
+Vytvořte složku: tests/exercises a v ní: page_objects_exercise.spec.ts vytvořte nový test: PageObjects Exercise - Login and Logout
+Ve složce src/pages/pmtool Vytvořte nový Page Object pro domovskou stránku (dashboard_page.ts).
+Page Object bude obsahovat:
+Klik na profilovou sekci (clickProfile), lokátor: #user_dropdown
+Klik na odhlášení (clickLogout), lokátor: #logout
+Do testu vložte otevření PMTool, přihlášení a následně odhlášení.
+
 */
